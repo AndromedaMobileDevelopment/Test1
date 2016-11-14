15b8485f7d5aae7bb039042577370f00404cb813
antoniten <ksusha123195@gmail.com>
2016-11-14 20:35:52 +0300

    tyuiop

--- app/src/main/java/com/development/mobile/andromeda/test2/MainActivity.java
+++ app/src/main/java/com/development/mobile/andromeda/test2/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int i =0;
-        int o = 1;
-        String u;
+        int a = 5;
     }
 }
